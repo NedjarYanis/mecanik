2c6296d006d4714c7c59858a9ee6a8a2233502e7
NedjarYanis <yanisnedjar5@gmail.com>
2026-03-22 17:14:19 +0100

    Intégration du VRAI serveur Firebase Firestore

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'  <-- Commente ton App
-import Seed from './Seed.jsx' // <-- Importe Seed à la place
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import './index.css'
+import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Seed /> 
-  </React.StrictMode>,
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
